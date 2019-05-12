8216edab83b0b8b7709303965d1b13be68f21e9b
Nahuel Rigo <kenygarth@gmail.com>
2019-05-12 23:25:44 +0000

    Login simulando ser pantalla inicial, estructura sólo de testeo.

---
+++ vista/contenidos/login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Test</title>
+</head>
+<body>
+	<h1>Esta es la pantalla de login</h1>
+	<h3>Es un testeo, no va a haber login</h3>
+	<h4>Pero podriamos hacer un cartel que diga <br>
+		"Encontrá tu salón acá", y que esa sea la <br>
+		pantalla principal.
+	</h4>
+</body>
+</html>
