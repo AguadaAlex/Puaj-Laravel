96f850e6abdafebf7b8e626538cdb88efa0a320e
PABLO VAZQUEZ <pablovzqz768@gmail.com>
2019-05-13 18:07:06 -0300

    agregamos un comentario de prueba

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     //include "vista/plantilla.php";
 
     require_once "controlador/vistaControlador.php";
-
+//comentario
     $plantilla = new vistaControlador();
     $plantilla->obtener_plantilla_controlador();
 ?>
