72495445836d36774e76254a6fc2aa61a47bf41e
Nahuel Rigo <kenygarth@gmail.com>
2019-05-14 04:22:21 +0000

    Archivo que marca la vista de los institutos, sin contar el "login" esta sería la primer página.

---
+++ vista/contenidos/institutos.php
@@ -0,0 +1,26 @@
+<div class="searcher">
+	
+	<input type="search" placeholder= "Ingresar materia" class="">
+	<!--<input type="submit" value="Buscar">-->
+	<button class="btn btn-success"><i class="fas fa-search"></i></button>
+</div>
+<div class="botones_institutos">
+	<form action="institutos">
+		<!-- Cambiar action="institutos" por "carreras" cuando este terminado --->
+		<button id="ingenieria" class="btn-primary btn form-submit" >Instituto de Ingeniería y Agronomía</button>
+	</form>
+	<form action="">
+		<button id="salud" class="btn-primary btn form-submit">Instituto de Ciencias de la Salud</button>
+	</form>
+	<form action="">
+		<button id="sociales" class="btn-primary btn form-submit">Instituto de Ciencias Sociales y Administración</button>
+	</form>
+	<form action="">
+		<button id="iniciales" class="btn-primary btn form-submit">Instituto de Estudios Iniciales</button>
+	</form>
+	
+	<!--<input class="ingenieria "type="submit" value="Instituto de Ingeniería y Agronomía" >
+	<input type="submit" value="Instituto de Ciencias Sociales y Administración">
+	<input type="submit" value="Instituto de Ciencias de la Salud">
+	<input type="submit" value="Instituto de Estudios Iniciales">-->
+</div>
