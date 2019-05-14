3f80014249f72f8f990dac677db8f61fcd4d757e
Nahuel Rigo <kenygarth@gmail.com>
2019-05-14 04:23:20 +0000

    Actualizo login para corresponder con lo sumado al proyecto, tiene un boton que manda a institutos.

--- vista/contenidos/login.php
+++ vista/contenidos/login.php
@@ -1,14 +1,26 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Test</title>
+	<?php include "vista/modulos/head.php"; ?>
 </head>
 <body>
-	<h1>Esta es la pantalla de login</h1>
-	<h3>Es un testeo, no va a haber login</h3>
-	<h4>Pero podriamos hacer un cartel que diga <br>
-		"Encontrá tu salón acá", y que esa sea la <br>
-		pantalla principal.
-	</h4>
+	<div class="caja">
+
+		<!-- Cabecera --> 
+	<?php include "vista/modulos/cabecera.php"; ?>
+
+	<!-- Contenido de la página, dentro de la section wrapper --->
+	<section class="wrapper">
+	<h1>Bienvenido al sistema de busqueda de aulas</h1>
+	<h1>Por favor, toque la pantalla para continuar</h1>
+	<form action="institutos">
+    <button type="submit">Continuar</button>
+	</form>
+	
+	</section>
+	
+	<!-- Pie de página --->
+	<?php include "vista/modulos/pie.php"; ?>
+	</div>
 </body>
 </html>
