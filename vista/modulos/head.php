d27f9955ba2676867c707ec5b6dde086fc36ce03
Nahuel Rigo <kenygarth@gmail.com>
2019-05-14 04:24:55 +0000

    head con los datos requeridos

---
+++ vista/modulos/head.php
@@ -0,0 +1,11 @@
+<meta charset="UTF-8">
+	<title>buscador_unaj</title>
+	<link rel="stylesheet" type="text/css" href="vista/css/estilo.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link	rel="stylesheet"	type="text/css"	href="vista/css/estilo.css" media="screen	and	(max-device-width:	480px)"	/>
+	<link href="https://fonts.googleapis.com/css?family=Anton" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
