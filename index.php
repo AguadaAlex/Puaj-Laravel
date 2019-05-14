15fb17cdc2c4860948755a54f53ece8d721cf67d
PABLO VAZQUEZ <pablovzqz768@gmail.com>
2019-05-13 23:23:12 -0300

    Subi los archivos de fede , (la hoja de estilos esta en las vistas)

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once "core/configGeneral.php";
-    //include "vista/plantilla.php";
-
-    require_once "controlador/vistaControlador.php";
-//comentario
-    $plantilla = new vistaControlador();
-    $plantilla->obtener_plantilla_controlador();
-?>
